9f782ae7824141e95934f9f56108fe718cdfd256
Tomek Lipski <tomek.lipski@gmail.com>
2015-03-27 11:30:00 +0100

    update version to 1.0.18

--- test/TaxamoTest/Common.php
+++ test/TaxamoTest/Common.php
@@ -2,7 +2,7 @@
 abstract class TaxamoTestCase extends UnitTestCase {
    function getApi() {
         $swagger = new APIClient('SamplePrivateTestKey1', 'https://api.taxamo.com');
-        $swagger->sourceId = "TaxamoTestPHP/1.0.17";
+        $swagger->sourceId = "TaxamoTestPHP/1.0.18";
         return new Taxamo($swagger);
    }
 }
