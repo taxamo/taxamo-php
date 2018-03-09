f93c6e641a60bf1bc0f59bdc69397a247b995daa
Tomek Lipski <tomek.lipski@gmail.com>
2018-03-09 16:21:59 +0100

    Bump version to 2.0.6

--- lib/Taxamo.php
+++ lib/Taxamo.php
@@ -24,7 +24,7 @@ require(dirname(__FILE__) . '/Taxamo/Swagger.php');
 
 class Taxamo {
 
-    public static $VERSION = "2.0.5";
+    public static $VERSION = "2.0.6";
 
 	function __construct($apiClient) {
 	  $this->apiClient = $apiClient;
--- test/TaxamoTest/Common.php
+++ test/TaxamoTest/Common.php
@@ -3,7 +3,7 @@
 abstract class TaxamoTestCase extends UnitTestCase {
    function getApi() {
         $swagger = new Taxamo\APIClient('SamplePrivateTestKey1', 'https://api.taxamo.com');
-        $swagger->sourceId = "TaxamoTestPHP/2.0.5";
+        $swagger->sourceId = "TaxamoTestPHP/2.0.6";
         return new Taxamo\Taxamo($swagger);
    }
 }
